10c70c6cc95aae8cd4db4cec579a1c59d533dcc4
AmirMmd <amirmmd.stu.nit.ac.ir>
2023-04-21 04:21:36 +0430

    last

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.html", "./src/components/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
